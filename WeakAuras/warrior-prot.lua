43e60bb56aeda884a775dac403b54e93772dfa58
phyltr <statik@cloned.org>
2017-02-03 11:44:40 -0700

    adding lua to see what changes better

---
+++ WeakAuras/warrior-prot.lua
@@ -0,0 +1,160 @@
+{
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["controlledChildren"] = {
+        [1] = "Thunder Clap1",
+        [2] = "Thunder Clap2",
+        [3] = "Neltharion's Fury",
+        [4] = "Neltharion's Fury CD",
+        [5] = "demoshout",
+        [6] = "demo shout",
+        [7] = "Shield Wall11",
+        [8] = "Shield Wall12",
+        [9] = "Last Stand 11",
+        [10] = "Last Stand 12",
+        [11] = "Impending Victory",
+        [12] = "impending victory2",
+        [13] = "Revenge 1",
+        [14] = "Revenge 2",
+        [15] = "Revenge 3",
+        [16] = "Revenge 4",
+        [17] = "vengeance: revenge",
+        [18] = "Focused Rage",
+        [19] = "Focused Rage CD",
+        [20] = "ShieldSlam 1",
+        [21] = "ShieldSlam2",
+        [22] = "Shield Block stack 22",
+        [23] = "Shield Block stack 3",
+        [24] = "Shield Block2",
+        [25] = "Shield Block CD",
+        [26] = "Ignore Pain",
+        [27] = "Ignore Pain CD",
+        [28] = "Ignore Pain Duration",
+        [29] = "Ignore Pain CD2",
+        [30] = "StormBolt 3",
+        [31] = "StormBolt 2 2",
+        [32] = "Shockwavee 2 2",
+        [33] = "Shockwavee 3",
+        [34] = "Last Stand 1",
+        [35] = "Last Stand 2",
+        [36] = "Last Stand 13",
+        [37] = "Shield Wall1",
+        [38] = "Shield Wall2",
+        [39] = "Shield Wall 13",
+        [40] = "Rage 3 2",
+        [41] = "Battle Cry CD",
+        [42] = "Battle Cry",
+        [43] = "Battle Cry Duration",
+        [44] = "Avatar2 2 2",
+        [45] = "Avatar1 2 2",
+        [46] = "AVatar3 2 2",
+        [47] = "avatar4 2 2",
+        [48] = "Taunt",
+        [49] = "Blood Fury",
+        [50] = "Warrior-Protection Ignore Pain v1.10",
+        [51] = "Vengeance: Ignore Pain",
+    },
+    ["borderBackdrop"] = "Blizzard Tooltip",
+    ["xOffset"] = 0,
+    ["border"] = false,
+    ["yOffset"] = -80.999633789063,
+    ["regionType"] = "group",
+    ["borderSize"] = 16,
+    ["borderColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 0.5,
+    },
+    ["expanded"] = true,
+    ["activeTriggerMode"] = 0,
+    ["borderOffset"] = 5,
+    ["id"] = "Warrior - Prot",
+    ["selfPoint"] = "BOTTOMLEFT",
+    ["additional_triggers"] = {
+    },
+    ["anchorPoint"] = "CENTER",
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["finish"] = {
+        },
+        ["init"] = {
+        },
+    },
+    ["borderInset"] = 11,
+    ["numTriggers"] = 1,
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+    },
+    ["trigger"] = {
+        ["unit"] = "player",
+        ["type"] = "aura",
+        ["spellIds"] = {
+        },
+        ["subeventSuffix"] = "_CAST_START",
+        ["debuffType"] = "HELPFUL",
+        ["names"] = {
+        },
+        ["event"] = "Health",
+        ["subeventPrefix"] = "SPELL",
+    },
+    ["borderEdge"] = "None",
+    ["load"] = {
+        ["talent"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["single"] = "WARRIOR",
+        },
+        ["difficulty"] = {
+            ["multi"] = {
+            },
+        },
+        ["role"] = {
+            ["multi"] = {
+            },
+        },
+        ["pvptalent"] = {
+            ["multi"] = {
+            },
+        },
+        ["faction"] = {
+            ["multi"] = {
+            },
+        },
+        ["use_class"] = "true",
+        ["race"] = {
+            ["multi"] = {
+            },
+        },
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["untrigger"] = {
+    },
+}
